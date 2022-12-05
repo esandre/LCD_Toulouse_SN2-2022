46c3003f20899fc647821319905f1e106b988b2a
Enzo Sandré <atelier@enzosandre.fr>
2022-12-05 10:46:59 +0100

    Ajout de cas pour VerticalStretch

--- LCD.Test/Taille2Test.cs
+++ LCD.Test/Taille2Test.cs
@@ -1,28 +1,30 @@
-﻿namespace LCD.Test
+﻿using LCD.Test.Utilities;
+
+namespace LCD.Test
 {
     public class Taille2Test
     {
-        [Fact]
-        public void Test1()
+        private static readonly int[] NombresATester = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 10, 123456789 };
+        private static readonly ushort[] EtirementsVerticauxATester = new ushort[] { 2, 3, ushort.MaxValue };
+
+        public static readonly IEnumerable<object> CasTestVerticalStretch
+            = new CartesianData(NombresATester, EtirementsVerticauxATester);
+
+        [Theory]
+        [MemberData(nameof(CasTestVerticalStretch))]
+        public void TestVerticalStretch(int nombre, ushort verticalStretch)
         {
-            // ETANT DONNE le chiffre 1
-            const int chiffre = 1;
+            // ETANT DONNE le nombre <nombre>
+            // ET un afficheur LCD taille verticale <verticalStretch>
+            var afficheur = new AfficheurLCD(tailleVerticale: verticalStretch);
 
-            // ET un afficheur LCD taille verticale 2
-            var afficheur = new AfficheurLCD(tailleVerticale: 2);
+            // QUAND on le convertir en LCD taille 1*<verticalStretch>
+            var lcd = afficheur.Convert(nombre);
 
-            // QUAND on le convertir en LCD taille 1*2
-            var lcd = afficheur.Convert(chiffre);
+            // ALORS on obtient un <nombre> étiré <verticalStretch> fois en hauteur
+            var aimedNumber = AfficheurLCD.ConvertWithDefaultSize(nombre);
 
-            // ALORS on obtient 4 barres verticales à droite
-            Assert.Equal(
-                "   " + Environment.NewLine +
-                "   " + Environment.NewLine +
-                "  |" + Environment.NewLine +
-                "  |" + Environment.NewLine +
-                "  |" + Environment.NewLine +
-                "  |" + Environment.NewLine, 
-                lcd);
+            Assert.Equal(aimedNumber.DuplicateEachLineVertically(verticalStretch), lcd);
         }
     }
 }
---
+++ LCD.Test/Utilities/StringLineExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+namespace LCD.Test.Utilities
+{
+    internal static class StringLineExtensions
+    {
+        public static string DuplicateEachLineVertically(this string str, ushort times)
+        {
+            var builder = new StringBuilder();
+
+            foreach (var line in str.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries))
+                for (var i = 0; i < times; i++)
+                    builder.AppendLine(line);
+
+            return builder.ToString();
+        }
+    }
+}
