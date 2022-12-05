1eeac2825eff4a88b203cf7c75701ca8f1cf48cb
Enzo Sandré <atelier@enzosandre.fr>
2022-12-05 10:17:59 +0100

    Premier test en taille 2

---
+++ LCD.Test/Taille2Test.cs
@@ -0,0 +1,28 @@
+﻿namespace LCD.Test
+{
+    public class Taille2Test
+    {
+        [Fact]
+        public void Test1()
+        {
+            // ETANT DONNE le chiffre 1
+            const int chiffre = 1;
+
+            // ET un afficheur LCD taille verticale 2
+            var afficheur = new AfficheurLCD(tailleVerticale: 2);
+
+            // QUAND on le convertir en LCD taille 1*2
+            var lcd = afficheur.Convert(chiffre);
+
+            // ALORS on obtient 4 barres verticales à droite
+            Assert.Equal(
+                "   " + Environment.NewLine +
+                "   " + Environment.NewLine +
+                "  |" + Environment.NewLine +
+                "  |" + Environment.NewLine +
+                "  |" + Environment.NewLine +
+                "  |" + Environment.NewLine, 
+                lcd);
+        }
+    }
+}
--- LCD/AfficheurLCD.cs
+++ LCD/AfficheurLCD.cs
@@ -2,24 +2,53 @@
 
 namespace LCD
 {
-    public static class AfficheurLCD
+    public class AfficheurLCD
     {
-        public static string Convert(int nombre) =>
-            nombre switch
-            {
-                0 => Digits.Zero,
-                1 => Digits.One,
-                2 => Digits.Two,
-                3 => Digits.Three,
-                4 => Digits.Four,
-                5 => Digits.Five,
-                6 => Digits.Six,
-                7 => Digits.Seven,
-                8 => Digits.Eight,
-                9 => Digits.Nine,
-                > 9 => Combine(Convert(nombre / 10), Convert(nombre % 10)),
-                _ => throw new NotImplementedException()
-            };
+        private readonly ushort _tailleVerticale;
+        private static readonly AfficheurLCD Defaut = new();
+
+        private AfficheurLCD() : this(1)
+        {
+        }
+
+        public AfficheurLCD(ushort tailleVerticale)
+        {
+            _tailleVerticale = tailleVerticale;
+        }
+
+        public static string ConvertWithDefaultSize(int nombre)
+            => nombre switch
+               {
+                   0   => Digits.Zero,
+                   1   => Digits.One,
+                   2   => Digits.Two,
+                   3   => Digits.Three,
+                   4   => Digits.Four,
+                   5   => Digits.Five,
+                   6   => Digits.Six,
+                   7   => Digits.Seven,
+                   8   => Digits.Eight,
+                   9   => Digits.Nine,
+                   > 9 => Combine(ConvertWithDefaultSize(nombre / 10), ConvertWithDefaultSize(nombre % 10)),
+                   _   => throw new NotImplementedException()
+               };
+
+        public string Convert(int nombre)
+        {
+            var taille1 = ConvertWithDefaultSize(nombre);
+            return StretchVertical(taille1);
+        }
+
+        private string StretchVertical(string block)
+        {
+            var builder = new StringBuilder();
+
+            foreach (var line in block.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries))
+                for (var i = 0; i < _tailleVerticale; i++)
+                    builder.AppendLine(line);
+
+            return builder.ToString();
+        }
 
         private static string Combine(string blockA, string blockB)
         {
